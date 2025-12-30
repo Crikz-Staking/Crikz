ad567b4583d7d7e8aa0b9adb8a3a3ce93037e4f0
Crikz <etritcocaj@proton.me>
2025-12-30 16:29:13 +0200

    Update vite.config.ts

--- dApp/vite.config.ts
+++ dApp/vite.config.ts
@@ -23,7 +23,3 @@ export default defineConfig({
     chunkSizeWarningLimit: 2000, // Increased for AI models
     target: 'esnext' // Required for Top-level await
   },
-  optimizeDeps: {
-    exclude: ['@mlc-ai/web-llm', '@xenova/transformers']
-  }
-})
